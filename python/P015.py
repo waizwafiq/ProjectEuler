da36a6a95bcf5708e9b5edd97ea644ec74b9c16f
Waiz Wafiq <63910911+waizwafiq@users.noreply.github.com>
2021-03-01 08:17:42 +0800

    Update P015.py

--- python/P015.py
+++ python/P015.py
@@ -1,3 +1,7 @@
+'''PROBLEM 015
+Starting in the top left corner of a 2×2 grid, and only being able to move to the right and down, there are exactly 6 routes to the bottom right corner.
+How many such routes are there through a 20×20 grid?
+'''
 from math import factorial as fact
 
 w,h = 20,20
